fee00caa0c76a2290d250f89cd244840c0075681
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2024-12-14 22:45:02 +0100

    some errors

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
+require("dotenv").config(); 
 const express = require("express");
 const dbconnection = require("./config/db");
 const authrouter = require('./routes/authRoutes');
 const cookieparser = require('cookie-parser');
-require("dotenv").config(); 
 const app = express();
 app.use(express.json());
 app.use(cookieparser()); 
 dbconnection.on('connected', 
     () => {
         app.use(authrouter);
-        app.all('*',(req,res)=> res.status(404) ) ; 
+        app.all('*', (req, res) => 
+            res.status(404).json({ message: 'Route not found' })
+        );
             app.listen(process.env.port, () => {
                 console.log('Listening on port', process.env.port);
             });
