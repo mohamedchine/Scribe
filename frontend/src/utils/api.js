6508955629a351aae9bb2621732d63a265b79cdd
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2026-03-07 03:36:42 +0000

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     withCredentials: true,
-
 });
 
 export default api;
